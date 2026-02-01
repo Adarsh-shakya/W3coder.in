270d7ad59e8f27ebb09515db07da07276eefda85
Adarsh Shakya <adarsh.shakya_cs20@gla.ac.in>
2026-02-01 20:34:21 +0530

    can_build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react() // Dekho, yahan se babel wala part maine hata diya hai
   ],
 })
