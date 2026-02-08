dc3dcc1186671203b2028a153dcbd0b4774a9f3d
Adarsh Shakya <adarsh.shakya_cs20@gla.ac.in>
2026-02-08 21:44:29 +0530

    navUpt

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import '../Styles/Navbar.css';
 
-const Navbar = () => { 
+const Navbar = () => {
 
   // ★ UPDATE: Links ab '/course/subject' format me hain
   const navLinks = [
-    { name: 'Java', path: '/course/java' },        // Old: /java
-    { name: 'Python', path: '/course/python' },    // Old: /python/fundamentals
-    { name: 'HTML', path: '/course/html' },        // Old: /html/intro
-    { name: 'CSS', path: '/course/css' },          
+    { name: 'Java', path: '/course/java' },
+    { name: 'Python', path: '/course/python' },
+    { name: 'HTML', path: '/course/html' },
+    { name: 'CSS', path: '/course/css' },
     { name: 'JavaScript', path: '/course/javascript' }
   ];
-  
+
   const [menuActive, setMenuActive] = useState(false);
 
   const toggleMenu = () => {
@@ -22,21 +22,23 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        
+
         <div className="navbar-logo">
           <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <h1> <span style={{color:"#f39c12",fontStyle:"italic"}}>W3</span>coder</h1>
-            
+            <h1> <span style={{ color: "#f39c12", fontStyle: "italic" }}>W3</span>coder</h1>
           </Link>
         </div>
 
-        <button className="navbar-toggle" onClick={toggleMenu}>
+        {/* <button className="navbar-toggle" onClick={toggleMenu}>
           &#9776;
-        </button>  
+        </button> */}
 
         <ul className={`navbar-menu ${menuActive ? 'active' : ''}`}>
+           <li className="navbar-toggle" onClick={toggleMenu}>
+          &#9776;
+        </li> 
           {navLinks.map((item) => (
-            <li key={item.name} onClick={() => setMenuActive(false)}> 
+            <li key={item.name} onClick={() => setMenuActive(false)}>
               <Link to={item.path}>
                 {item.name}
               </Link>
